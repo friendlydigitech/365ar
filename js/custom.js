a83941199c5bb66c8e5a2a5e960ba2fc16e21eeb
kift <su.shihjie@gmail.com>
2014-06-12 09:53:06 +0800

    關閉預設提供的form js處理

--- js/custom.js
+++ js/custom.js
@@ -108,39 +108,39 @@ $(document).ready(function() {
 
 /* ==== 7) Form ==== */
 
-	$('.flowuplabels').FlowupLabels({
-	        feature_onInitLoad: false, 
-	        class_focused:      'focused',
-	        class_populated:    'populated' 
-	});
+	// $('.flowuplabels').FlowupLabels({
+	//         feature_onInitLoad: false, 
+	//         class_focused:      'focused',
+	//         class_populated:    'populated' 
+	// });
 
-	var options = {
-		target: '.message .alert',
-		beforeSubmit: showRequest,
-		success: showResponse
-		};
+	// var options = {
+	// 	target: '.message .alert',
+	// 	beforeSubmit: showRequest,
+	// 	success: showResponse
+	// 	};
 
-	$('#contactForm').ajaxForm(options);
+	// $('#contactForm').ajaxForm(options);
 
-	function showRequest(formData, jqForm, options) {
-		var queryString = $.param(formData);
-			return true;
-		}
-	function showResponse(responseText, statusText)  {
-		}
-	$.fn.clearForm = function() {
-		return this.each(function() {
-			var type = this.type, tag = this.tagName.toLowerCase();
-			if (tag == 'form')
-				return $(':input',this).clearForm();
-			if (type == 'text' || type == 'password' || tag == 'textarea')
-				this.value = '';
-			else if (type == 'checkbox' || type == 'radio')
-				this.checked = false;
-			else if (tag == 'select')
-				this.selectedIndex = -1;
-			});
-		};
+	// function showRequest(formData, jqForm, options) {
+	// 	var queryString = $.param(formData);
+	// 		return true;
+	// 	}
+	// function showResponse(responseText, statusText)  {
+	// 	}
+	// $.fn.clearForm = function() {
+	// 	return this.each(function() {
+	// 		var type = this.type, tag = this.tagName.toLowerCase();
+	// 		if (tag == 'form')
+	// 			return $(':input',this).clearForm();
+	// 		if (type == 'text' || type == 'password' || tag == 'textarea')
+	// 			this.value = '';
+	// 		else if (type == 'checkbox' || type == 'radio')
+	// 			this.checked = false;
+	// 		else if (tag == 'select')
+	// 			this.selectedIndex = -1;
+	// 		});
+	// 	};
 
 });
 
